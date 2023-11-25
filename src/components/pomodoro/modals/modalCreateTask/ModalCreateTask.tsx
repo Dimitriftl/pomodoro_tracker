01cf185606989450733e068b66fc000f8a0b7c9a
Dimitri Fontenelle <dimitriftl@hotmail.com>
2023-11-25 14:46:09 +0100

    :construction: :lipstick: modalCreateTask css

--- src/components/pomodoro/modals/modalCreateTask/ModalCreateTask.tsx
+++ src/components/pomodoro/modals/modalCreateTask/ModalCreateTask.tsx
@@ -1,5 +1,13 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, {
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import "./modalCreateTask.scss";
+import { ThemeContext } from "../../../../context/MyProviders";
+import { CrossSvg } from "../../../../assets/svg/svg";
 
 type modalProps = {
   modal: boolean;
@@ -7,11 +15,64 @@ type modalProps = {
 };
 
 const ModalCreateTask = ({ modal, setModal }) => {
+  // context state
+  const { themeColor } = useContext(ThemeContext);
+  const [description, setDescription] = useState<string>("");
+  const areaMaxLength = 120;
+
+  // handle la fermerture du modal avec la touche escape / esc
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if (e.key === "Escape" || e.keyCode === 27) {
+        setModal(!modal);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      if (!modal) {
+        window.removeEventListener("keydown", handleKeyPress);
+      }
+    };
+  }, [modal]);
   return (
     <>
       <div className="overlay" onClick={() => setModal(!modal)}></div>
-      <div className="modalContent">
-        <h2>modal</h2>
+      <div className="modalCreateTaskContent background">
+        <div id="modalHeader">
+          <h2>Create a new task.</h2>
+          <button id="closeModal" onClick={() => setModal(!modal)}>
+            <CrossSvg theme={themeColor} />
+          </button>
+        </div>
+        <form id="createTaskForm">
+          <input
+            type="text"
+            name="taskName"
+            id="taskName"
+            placeholder="Add a title"
+          />
+          <div id="textareaContainer">
+            {/* créer un conteur de caractères */}
+            <textarea
+              maxLength={areaMaxLength}
+              name="taskDesciption"
+              id="taskDescription"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Description"
+            />
+            <p id="caracCounter">
+              {description.length}/{areaMaxLength}
+            </p>
+          </div>
+          <div id="modalFooter">
+            <button id="validate" className="backgroundBlue" type="submit">
+              <p>valider</p>
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
--- src/views/calendar/Header/Header.tsx
+++ src/views/calendar/Header/Header.tsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { PlusSvg } from "../../../assets/svg/svg";
 import "./header.scss";
+import ModalCreateTask from "../../../components/pomodoro/modals/modalCreateTask/ModalCreateTask";
 
 type Props = {};
 
 const Header = () => {
+  const [modal, setModal] = useState<boolean>(false);
   return (
-    <header>
-      <h2>Calendar</h2>
-      <button id="headerButton" className="backgroundBlue">
-        <PlusSvg width={16} height={16} />
-        Create a new task
-      </button>
-    </header>
+    <>
+      <header>
+        <h2>Calendar</h2>
+        <button
+          id="headerButton"
+          className="backgroundBlue"
+          onClick={() => setModal(!modal)}>
+          <PlusSvg width={16} height={16} />
+          Create a new task
+        </button>
+      </header>
+      {modal && <ModalCreateTask modal={modal} setModal={setModal} />}
+    </>
   );
 };
 
