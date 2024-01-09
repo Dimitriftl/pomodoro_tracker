d6f359f5be01f81cbca69c2b4435bbbfa05efb0e
Fontenelle Dimitri <dimitriftl@hotmail.com>
2024-01-09 15:11:11 +0100

    :construction: account right part page inputs

--- src/views/account/Account.tsx
+++ src/views/account/Account.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import "./account.scss";
-import editSvg from "../../assets/svg/accountPage/edit.svg"
+import editSvg from "../../assets/svg/accountPage/edit.svg";
 
 const Account = () => {
-
   return (
     <div id="accountContainer">
       <div id="accountContent">
@@ -11,14 +10,13 @@ const Account = () => {
           <h2>Account setting</h2>
         </div>
         <div id="accountLeftPart">
-            <h3>Profile picture</h3>
+          <h3>Profile picture</h3>
           <div id="accountImgContainer">
             <div id="accountImgoverlay">
-                <div id="accountImgoverlayText">
-                    <img id="editIcon"src={editSvg} alt="edit icon"/>
-                    <p>Upload a new picture</p>
-
-                </div>
+              <div id="accountImgoverlayText">
+                <img id="editIcon" src={editSvg} alt="edit icon" />
+                <p>Upload a new picture</p>
+              </div>
             </div>
             <img
               src="https://via.placeholder.com/150"
@@ -26,8 +24,27 @@ const Account = () => {
             />
           </div>
         </div>
-
-        <div id="accountRightPart"></div>
+        <div id="accountRightPart">
+          <div className="inputContainer">
+            <p className="inputLabel">Name</p>
+            <input type="text" placeholder="John Doe" />
+          </div>
+          <div className="inputContainer">
+            <p className="inputLabel">Email</p>
+            <input type="email" placeholder="John.doe@mail.com" />
+          </div>
+          <div className="inputContainer">
+            <p className="inputLabel">Phone</p>
+            <input type="number" placeholder="+33 6 00 00 00 00" />
+          </div>
+          <div className="inputContainer">
+            <p className="inputLabel">Password</p>
+            <input type="password" readonly placeholder="***************" />
+            <button id="accountEditPasswordButton">
+              <img src={editSvg} alt="" /> edit
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
