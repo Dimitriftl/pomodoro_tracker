2234a7bfd887a1a72a5fa70d6e997bc012297376
Fontenelle Dimitri <dimitriftl@hotmail.com>
2024-01-10 17:06:39 +0100

    :lipstick: starting dashboard page

--- src/views/dashboard/index/index.tsx
+++ src/views/dashboard/index/index.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./index.scss";
+
+type detailsDisplayed = "hours" | "tasks";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <h1>Dashboard</h1>
+  const [detailsDisplayed, setDetailsDisplayed] =
+    useState<detailsDisplayed>("hours");
+
+  return (
+    <div id="dashboardContainer">
+      <div id="dashboardContent">
+        <div id="dashboardCardsContainer">
+          <div
+            className={
+              detailsDisplayed === "hours"
+                ? "cardTotalInfo cardSelected"
+                : "cardTotalInfo"
+            }
+            onClick={() => setDetailsDisplayed("hours")}
+          >
+            <h2>Total hours worked</h2>
+          </div>
+          <div
+            className={
+              detailsDisplayed === "tasks"
+                ? "cardTotalInfo cardSelected"
+                : "cardTotalInfo"
+            }
+            onClick={() => setDetailsDisplayed("tasks")}
+          >
+            <h2>Total Tasks worked</h2>
+          </div>
         </div>
-    );
+        <div id="dashboardDetailsContainer">
+          {detailsDisplayed === "hours" ? (
+            <h2>hours details</h2>
+          ) : (
+            <h2>tasks details</h2>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
