a5e85be79facda091c0f9c01896f5628e002584f
Dimitri Fontenelle <dimitriftl@hotmail.com>
2024-03-09 09:18:17 +0100

    define useBackEndRouteTypes

---
+++ src/utils/types/useBackendRouteTypes.ts
@@ -0,0 +1,12 @@
+export type registerRelatedTypes = "signUp" | "signIn";
+
+export type pomodoroRelatedTypes =
+  | "updatePomodoroDoneAndTimeSpend"
+  | "updateTimeSpend"
+  | "createTask"
+  | "editTask"
+  | "giveUpTask"
+  | "deleteTask"
+  | "taskDone";
+
+export type accountRelatedTypes = "updatePassword" | "updateUserInformations";
